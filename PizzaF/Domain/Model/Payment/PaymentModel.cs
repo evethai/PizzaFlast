6baeb8396a78b3aff2d7b4aea3f375ba1c49aef9
dangkhoaaaa <dangkhoa3348@gmail.com>
2024-06-18 13:04:49 +0700

    payment method

---
+++ PizzaF/Domain/Model/Payment/PaymentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Payment
+{
+    public class PaymentModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid? OrderId { get; set; }
+
+        public double? Amount { get; set; }
+
+        public string? Method { get; set; }
+
+        public string? Status { get; set; }
+
+        //  public virtual Order? Order { get; set; }
+    }
+
+}
